c5852553eb83bc282afedd0f8a718ceaf9b98e59
Erinn Neidinger <erinndneidinger@gmail.com>
2020-01-28 14:14:06 -0600

    created roles for admin and teammember

--- FinalCapstone/FinalCapstone/Startup.cs
+++ FinalCapstone/FinalCapstone/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using FinalCapstone.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(FinalCapstone.Startup))]
@@ -9,6 +12,52 @@ namespace FinalCapstone
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup I am creating first Admin Role and creating a default Admin User   
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin role   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                 
+
+                var user = new ApplicationUser();
+                user.UserName = "shanu";
+                user.Email = "syedshanumcain@gmail.com";
+
+                string userPWD = "A@Z200711";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin  
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role   
+            if (!roleManager.RoleExists("Teammember"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Teammember";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
